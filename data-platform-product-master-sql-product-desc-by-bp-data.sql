47ebe1aeb2ac6d621efd1a8a160df7fd8f842832
Yurino Oga <yurino.o@latona.io>
2022-12-09 07:39:57 +0900

    release

---
+++ data-platform-product-master-sql-product-desc-by-bp-data.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `data_platform_product_master_product_desc_by_bp_data`
+(
+    `Product`               varchar(40) NOT NULL,
+    `BusinessPartner`       int(12) NOT NULL,
+    `Language`              varchar(2) NOT NULL,
+    `ProductDescription`    varchar(200) DEFAULT NULL,
+
+    PRIMARY KEY (`Product`, `BusinessPartner`, `Language`),
+
+    CONSTRAINT `DataPlatformProductMasterProductDescByBPData_fk` FOREIGN KEY (`Product`, `BusinessPartner`) REFERENCES `data_platform_product_master_business_partner_data` (`Product`, `BusinessPartner`),
+    CONSTRAINT `DataPlatformProductMasterProductDescByBPDataLanguage_fk` FOREIGN KEY (`Product`, `Language`) REFERENCES `data_platform_product_master_product_description_data` (`Product`, `Language`)
+
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
